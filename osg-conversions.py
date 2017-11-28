f1e923f78283bd58f6b9a18e806635d7e13c3ac9
Brian Lin <brianhlin@gmail.com>
2017-11-28 09:44:13 -0600

    Take optional link map file option for converting links

--- osg-conversions.py
+++ osg-conversions.py
@@ -5,17 +5,45 @@ import sys
 
 twiki = sys.stdin.read()
 
-twiki = re.sub('``` screen', '``` console', twiki)
-twiki = re.sub('``` rootscreen', '``` console', twiki)
-twiki = re.sub('<span class="twiki-macro TOC".*></span>\n', '', twiki)
-twiki = re.sub('<span class="twiki-macro UCL\_PROMPT\_ROOT"></span>', '[root@client ~] $', twiki)
-twiki = re.sub('<span class="twiki-macro UCL\_PROMPT"></span>', '[user@client ~] $', twiki)
-twiki = re.sub('%UCL_PROMPT_ROOT%', 'root@host #', twiki)
-twiki = re.sub('%UCL_PROMPT%', 'user@host $', twiki)
-twiki = re.sub('&lt;', '<', twiki)
-twiki = re.sub('&gt;', '>', twiki)
-twiki = re.sub('<span class="twiki-macro NOTE"></span>', '!!! note\n   ', twiki)
-twiki = re.sub('<span class="twiki-macro RED"></span> \*\*NOTE:\*\* <span class="twiki-macro ENDCOLOR"></span>',
-               '!!! note\n   ', twiki)
+replacements = [(r'``` screen', '``` console'),
+                (r'``` rootscreen', '``` console'),
+                (r'<span class="twiki-macro TOC".*></span>\n', ''),
+                (r'<span class="twiki-macro UCL\_PROMPT\_ROOT"></span>', '[root@client ~] $'),
+                (r'\[root@client ~\]\s*\$', 'root@host #'),
+                (r'<span class="twiki-macro UCL\_PROMPT"></span>', '[user@client ~] $'),
+                (r'\[user@client ~\]\s*\$', 'user@host $'),
+                (r'%UCL_PROMPT_ROOT%', 'root@host #'),
+                (r'%UCL_PROMPT%', 'user@host $'),
+                (r'&lt;', '<'),
+                (r'&gt;', '>'),
+                (r'<span class="twiki-macro NOTE"></span>', '!!! note\n   '),
+                (r'<span class="twiki-macro DOC_STATUS_TABLE"></span>\s*', ''),
+                (r'<span class="twiki-macro STARTSECTION">.*', ''),
+                (r'<span class="twiki-macro ENDSECTION">.*', ''),
+                (r'<span class="twiki-macro COMMENT" type="tableappend"></span>\s*', ''),
+                (r'-- Main\..*\n', ''),
+                (r'\\#[\w\s]+\n', ''),
+                (r'\*\*Comments\*\*\s*\n[-=\s]*', ''),
+                (r'^-*\\[#\s]*Comments\s*\n', ''),
+                (r'<span class="twiki-macro TWISTY">%TWISTY\\_OPTS\\_OUTPUT%</span>\s*', '<details>'),
+                (r'<span style="background-color: #\w+;">([^<]+)</span>', r'%RED%\1%ENDCOLOR%'),
+                (r'<span class="twiki-macro TWISTY.*showlink="([\w\s]*).*</span>', r'<details>\n  <summary>\1</summary>'),
+                (r'<span class="twiki-macro ENDTWISTY"></span>', '</details>'),
+                (r'<span class="twiki-macro RED"></span> \*\*NOTE:\*\* <span class="twiki-macro ENDCOLOR"></span>', '!!! note\n   ')]
+
+for args in replacements:
+    orig, sub = args
+    twiki = re.sub(orig, sub, twiki)
+
+
+# optionally take link mapping argument
+try:
+    with open(sys.argv[1], 'r') as f:
+        links = [x.split() for x in f.readlines()]
+
+    for link in links:
+        twiki = re.sub(r'\(%s\)' % link[0].split('/')[-1], r'(%s)' % link[1], twiki)
+except (IOError, IndexError):
+    pass
 
 print twiki
